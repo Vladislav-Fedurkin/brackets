308613d179679a345908a149feed6e9074445304
Vladislav-Fedurkin <vladislav.fedurkin@gmail.com>
2021-03-14 18:39:10 +0300

    feat: added solution to task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i<str.length; i++) {
+      for (let t = 0; t<bracketsConfig.length; t++) {
+      if (str.includes(bracketsConfig[t][0] + bracketsConfig[t][1])) {
+        str = str.replace(bracketsConfig[t][0] + bracketsConfig[t][1], "");
+        i--;
+      }
+    }
+  }
+  if (str.length != 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
